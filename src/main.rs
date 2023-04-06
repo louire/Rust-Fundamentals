700829aaad792384f2e448601d1fb09222814db4
louire <devlouirecio@gmail.com>
2023-04-05 19:41:41 -0500

    Wuhu

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,26 @@ fn fibonacci(n: u128) -> u128 {
 
 fn main() {
     println!("La suma de los numeros fibonacci 20 veces es {}", fibonacci(20));
-}
 
 
 //the fn fibonacci is recursive, it calls itself until it reaches the base case, which is when n is 0 or 1.
 //The base case returns the value 0 or 1, and the recursive case returns the sum of the previous two numbers.
+
+    println!("Your name? ");
+    let mut nombre = String::new();
+
+    std::io::stdin().read_line(&mut nombre).unwrap();
+    nombre = nombre.trim().to_string();
+
+    println!("Years? ");
+    let mut edad = String::new();
+    std::io::stdin().read_line(&mut edad).unwrap();
+    //Convertir edad de string a int
+    let edad_int : u8 = edad.trim().parse().unwrap();
+    
+
+    println!("Welcome {}, you have {} years", nombre, edad_int);
+
+
+}
+
